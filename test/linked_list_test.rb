4f9650d0d2597bbf185e63227da57197e9429f99
brickstar <brickstar@gmail.com>
2018-03-19 23:46:30 -0600

    working on find

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -88,7 +88,7 @@ class LinkedListTest < Minitest::Test
     ll.append("shi")
     ll.append("shu")
     ll.append("blop")
-
+# binding.pry
     assert_equal "deep woo shi shu blop", ll.to_string
     assert_equal "shi", ll.find(2, 1)
     assert_equal "woo shi shu", ll.find(1, 3)
@@ -102,7 +102,7 @@ class LinkedListTest < Minitest::Test
     ll.append("shi")
     ll.append("shu")
     ll.append("blop")
-    
+
     assert_equal 5, ll.node_data.length
     assert_equal true, ll.includes?("deep")
     assert_equal false, ll.includes?("dep")
