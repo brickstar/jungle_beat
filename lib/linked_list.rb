180b9e411969c9fbfb6fb7863c6b2f8006983d2d
brickstar <brickstar@gmail.com>
2018-03-19 19:20:36 -0600

    working on append and testing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,25 @@
+require './lib/node'
+
 class LinkedList
-  
+  attr_reader :head,
+              :count
+  def initialize
+    @head = nil
+    @count = 0
+  end
+
+  def append(data)
+    @count += 1
+    current_node = @head
+    if @head == nil
+      @head = Node.new(data)
+    else
+      until current_node.next_node == nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = Node.new(data)
+    end
+  end
+
+
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,9 +1,31 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/linked_list'
+require './lib/node'
+require 'pry'
 
 class LinkedListTest < Minitest::Test
   def test_it_exists
-    assert_instance_of LinkedList, LinkedList.new
+    ll = LinkedList.new
+
+    assert_instance_of LinkedList, ll
+  end
+
+  def test_it_has_a_head_and_head_points_to_nil
+    ll = LinkedList.new
+
+    assert_nil ll.head
+  end
+
+  def test_it_can_append_to_the_list
+    ll = LinkedList.new
+
+    assert_equal 'doop', ll.append("doop").data
+    assert_nil node.next_node
+
+  end
+
+  def test_to_string
+    ll = LinkedList.new
   end
 end
